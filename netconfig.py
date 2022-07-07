92652abb59a9f9146227492e7f9a0aa9807de0f5
Jackzy <jaka.smrkolj@students.vegova.si>
2022-07-07 12:02:55 +0200

    added interface display

--- netconfig.py
+++ netconfig.py
@@ -97,8 +97,6 @@ def listDevices():
 def displayHelp():
     with open("C:/Users/Jaka/Documents/Programming/Python/netconfig/help.txt", "r") as f:
         print(f.read())
-
-print("test")
     
 match ACTION:	
     case "1":	
